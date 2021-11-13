3acb36234e3934f1d2dd9466d00a8c3ab3489993
DipPal513 <dip.pal.513@gmail.com>
2021-11-14 00:13:09 +0600

    'astttt

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -41,7 +41,7 @@ const Footer = () => {
           <div className="container text-center text-md-start mt-5">
             <div className="row mt-3">
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold">Company name</h6>
+                <h6 className="text-uppercase fw-bold">Kay jewelrs</h6>
                 <hr
                   className="mb-4 mt-0 d-inline-block mx-auto"
                   style={{
